7241600f9a9c211a5f4ca263012fd5c012593f15
Evandro Lima <evandrolimabarbosa@hotmail.com>
2023-11-20 07:49:05 -0300

    Criando o array das portas

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import Porta from "../components/Porta";
 import PortaModel from "../model/porta";
 import styles from "../styles/Index.module.css";
+import { criarPortas } from "../functions/portas";
 
 export default function Home() {
   const [p1, setP1] = useState(new PortaModel(1));
+
+  console.log(criarPortas(3, 2))
   return (
     <div className={styles.div}>
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
     </div>
   );
 }
